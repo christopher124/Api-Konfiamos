c2d5625bc3fa3ddaa446d37c0bdc38597000c243
Christopher Sosa <urri_@hotmail.com>
2023-07-04 11:49:44 -0600

    Integracion de rutas

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ const { API_VERSION } = require("./constants");
 const app = express();
 
 /// Import Routings
+const authRoutes = require("./router/auth");
+const roleRoutes = require("./router/role");
+const userRoutes = require("./router/user");
+const customerRoutes = require("./router/customer");
+const menuRoutes = require("./router/menu");
+const loanRequestRoutes = require("./router/loanRequest");
+const paymentRoutes = require("./router/payment");
+const groupLoanRequest = require("./router/groupLoanRequest");
 
 // Configure Body Parse
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -18,5 +26,13 @@ app.use(express.static("uploads"));
 app.use(cors());
 
 // Configure routings
+app.use(`/api/${API_VERSION}`, authRoutes);
+app.use(`/api/${API_VERSION}`, roleRoutes);
+app.use(`/api/${API_VERSION}`, userRoutes);
+app.use(`/api/${API_VERSION}`, menuRoutes);
+app.use(`/api/${API_VERSION}`, loanRequestRoutes);
+app.use(`/api/${API_VERSION}`, customerRoutes);
+app.use(`/api/${API_VERSION}`, paymentRoutes);
+app.use(`/api/${API_VERSION}`, groupLoanRequest);
 
 module.exports = app;
