2471286933a927a2e7f3283623da8deeedd5387c
Christopher Sosa <urri_@hotmail.com>
2023-07-04 11:45:20 -0600

    Ruta de clientes

---
+++ router/customer.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const multiparty = require("connect-multiparty");
+const CustomerController = require("../controllers/customer");
+const md_auth = require("../middlewares/authenticated");
+
+// Middleware para subir documetos del cliente
+const md_upload = multiparty({
+  uploadDir: "./uploads/documents",
+});
+
+const api = express.Router();
+
+// Crear un nuevo cliente
+api.post(
+  "/customer",
+  [md_auth.asureAuth, md_upload],
+  CustomerController.createCustumer
+);
+// actulizar un cliente
+api.patch(
+  "/customer/:id",
+  [md_auth.asureAuth, md_upload],
+  CustomerController.updateCustumer
+);
+// Eliminar un cliente
+api.delete(
+  "/customer/:id",
+  [md_auth.asureAuth],
+  CustomerController.deleteCustomer
+);
+// recuperacion de cliente
+api.put(
+  "/customer/:id/restore",
+  [md_auth.asureAuth],
+  CustomerController.restoreCustomer
+);
+// Obtener todos los cliente
+api.get("/customers", [md_auth.asureAuth], CustomerController.getCustomers);
+// Obtener todos los cliente eliminados
+// api.get(
+//   "/customersdeleted",
+//   [md_auth.asureAuth],
+//   CustomerController.getDeletedCustomers
+// );
+
+// Obtener un cliente
+api.get("/customer/:id", [md_auth.asureAuth], CustomerController.getCustomer);
+
+module.exports = api;
